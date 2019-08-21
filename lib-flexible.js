cab5e2340a8e0ef241f68c93a3d618743a2c62df
程金山 <1006153144@qq.com>
2019-08-21 23:54:39 +0800

    Update lib-flexible.js

--- lib-flexible.js
+++ lib-flexible.js
@@ -1,3 +1,16 @@
+/*
+px 转化换 rem ，转化是 10 。
+比如：
+你的设计图为750，那么就 750 / 75 = 10 rem。
+设计图中你量尺寸都要除 75 就是 rem值。再比如量的设计图按钮宽度 66px，那么计算：66 / 75 = 0.88rem 
+
+比如2：
+设计图为900，那么 900 / 90 = 10rem;
+设计图中所有量的尺寸都得除 90 就等于 rem的之。
+
+以此类推
+*/
+
 (function flexible (window, document) {
   var docEl = document.documentElement
   var dpr = window.devicePixelRatio || 1
